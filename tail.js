dbcf2a5ba26e532946b1743d9d992529a3a9095e
Adam McPhee <adkmcphee@gmail.com>
2023-04-10 18:56:16 +0000

    refactored tail with module export

--- tail.js
+++ tail.js
@@ -1,33 +1,7 @@
-// Assert Equal Function
-const assertEqual = function(actual, expected) {
-  let passEmoji = "🥳🥳🥳🥳🥳";
-  let failEmoji = "😡😡😡😡😡";
-  if (actual === expected) {
-    console.log(`${passEmoji} Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`${failEmoji} Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-
-
-
-
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-
-
+module.exports = tail;
