4c20abb4e691357d11bd32c75618f5bd7d4d6c8a
Adam McPhee <adkmcphee@gmail.com>
2023-04-10 21:54:14 +0000

    added assertArraysEqual test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,9 @@
 const middle = require('../middle')
+const assertArraysEqual = require('../assertArraysEqual');
 
-console.log(middle([1, 2, 3])); // => [2]
+const value = middle([1, 2, 3])
+
+console.log(assertArraysEqual(value, [2])); // => [2]
 console.log(middle([1, 2, 3, 4, 5])); // => [3]
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
