38f650ef89487226d53b3aa3343d2fc311088d2c
Adam McPhee <adkmcphee@gmail.com>
2023-04-10 19:16:44 +0000

    made eqArrays required, moved tests to test file, added module export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,4 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  //check if arrays are same length
-  if (arrayOne.length !== arrayTwo.length){
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++){
-    if (arrayOne[i] !== arrayTwo[i]){
-      return false;
-    }
-  } 
-  return true;
-  };
-
-  //  Create a function that will take in two arrays and console.log an appropriate message to the console.
-  // 
+const eqArrays = require('./eqArrays')
 
   const assertArraysEqual = function(arrayOne, arrayTwo) {
     if (eqArrays(arrayOne, arrayTwo) !== true){
@@ -21,5 +7,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
       return (`🥳🥳🥳🥳🥳 Assertion Passed: ${arrayOne} === ${arrayTwo}`)
   };
 
-console.log(assertArraysEqual([3, 2, 1], [3, 2, 1]));
-console.log(assertArraysEqual([3, 2, 1], [3, 2, "hello"]));
+module.exports = assertArraysEqual;
