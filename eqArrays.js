94f06223bc0cb418ac032fb157f44a1edadbcf3a
Adam McPhee <adkmcphee@gmail.com>
2023-04-10 19:09:29 +0000

    refactored with module export func

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,3 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`😡😡😡😡😡 Assertion Failed: ${actual} !==  ${expected}`);
-    return;
-  } 
-  //Happy Path
-    console.log(`🥳🥳🥳🥳🥳 Assertion Passed: ${actual} ===  ${expected}`);
-};
-
-// Create a function which takes in two arrays and returns true or false, based on a perfect match. Use assertEqual to test case for various scenarios. 
- 
-
 const eqArrays = function(arrayOne, arrayTwo) {
   //check if arrays are same length
   if (arrayOne.length !== arrayTwo.length){
